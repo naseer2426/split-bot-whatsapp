a8fa8e06b1944556481cffe3d86dfbab88825594
naseer2426 <naseerfathima2426@gmail.com>
2026-05-24 15:05:52 +0400

    added respond to @ in nanobot

--- internal/whatsapp/nanobot.go
+++ internal/whatsapp/nanobot.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 
 	"github.com/naseer2426/split-bot-whatsapp/internal/nanobot"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
 func (h *Handler) handleNanobotMode(evt *events.Message) string {
 	messageText := getMessageText(evt)
 	if messageText == "" {
+		return ""
+	}
+
+	if !h.shouldProcessNanobotMsg(evt, messageText) {
+		fmt.Printf("Group message from %s doesn't include bot_name '%s', skipping...\n", evt.Info.Sender, h.botName)
 		return ""
 	}
 
@@ -29,4 +35,11 @@ func (h *Handler) handleNanobotMode(evt *events.Message) string {
 
 	// Nanobot replies asynchronously via POST /send_message_to_chat.
 	return ""
+}
+
+func (h *Handler) shouldProcessNanobotMsg(evt *events.Message, messageText string) bool {
+	if evt.Info.Chat.Server != types.GroupServer {
+		return true
+	}
+	return h.messageContainsBotName(messageText)
 }
